6873c4728964972f2c0c10dd927470208f9e0efb
Robert Lee <bobbydwise1@hotmail.com>
2019-08-27 18:18:39 -0700

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Amazon.
+
+Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum length, return any one.
+
+For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
+*/
+
+console.log(rand5())
+
+$(document).ready(function() {
+  $('#form1').submit(function(event){
+    event.preventDefault()
+    let output0 = 1
+    console.log(output1)
+    $('#output-section-0').text(output0)
+  })
+});
